b83307c5cbab1675dc4b33a7e4d14c937d6599cb
Brett Canter <wonderbred@users.noreply.github.com>
2026-01-26 19:39:18 -0800

    Updated Ruckus FastIron to handle logging in with default credentials

--- scrapli_community/ruckus/fastiron/async_driver.py
+++ scrapli_community/ruckus/fastiron/async_driver.py
@@ -16,6 +16,13 @@ async def default_async_on_open(conn: AsyncNetworkDriver) -> None:
     Raises:
         N/A
     """
+    # When logging in with default credentials, you are prompted to change the password.
+    # To keep things simple, we just re-enter the default password for both prompts.
+    if conn.auth_username == "super" and conn.auth_password == "sp-admin":
+        conn.channel.write(channel_input="sp-admin")
+        conn.channel.send_return()
+        conn.channel.write(channel_input="sp-admin")
+        conn.channel.send_return()
     await conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
     await conn.send_command(command="skip-page-display")
 
--- scrapli_community/ruckus/fastiron/sync_driver.py
+++ scrapli_community/ruckus/fastiron/sync_driver.py
@@ -16,6 +16,13 @@ def default_sync_on_open(conn: NetworkDriver) -> None:
     Raises:
         N/A
     """
+    # When logging in with default credentials, you are prompted to change the password.
+    # To keep things simple, we just re-enter the default password for both prompts.
+    if conn.auth_username == "super" and conn.auth_password == "sp-admin":
+        conn.channel.write(channel_input="sp-admin")
+        conn.channel.send_return()
+        conn.channel.write(channel_input="sp-admin")
+        conn.channel.send_return()
     conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
     conn.send_command(command="skip-page-display")
 
